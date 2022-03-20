d69c57bebc2e387814e8c43acd79d0dc6676d8c3
JosueQ21 <jj21qm@gmail.com>
2022-03-20 13:19:07 -0700

    Sprint 9: Hallway fixes and misc
    Made the hallway door respond off of distance and a button press rather than collision. Also modified block and maze files to include distance so the player won't be warped to a mini game from anywhere of the classroom.

--- CIDSE - An Intelligent Tutoring in Virtual Reality/Assets/Scripts/ClassToBlock.cs
+++ CIDSE - An Intelligent Tutoring in Virtual Reality/Assets/Scripts/ClassToBlock.cs
@@ -9,6 +9,9 @@ public class ClassToBlock : XRBaseInteractable
 {
     public InputActionReference reference = null;
     public GameObject screen = null;
+    public GameObject player = null;
+    private float distance;
+    public float accDistance = 2;
 
     protected override void Awake()
     {
@@ -21,42 +24,16 @@ public class ClassToBlock : XRBaseInteractable
         reference.action.started -= SelectGame;
     }
 
+    private void Update()
+    {
+        distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+    }
+
     private void SelectGame(InputAction.CallbackContext context)
     {
-        if (screen.activeSelf)
+        if (distance < accDistance)
         {
             SceneManager.LoadScene(3);
         }
     }
-    /*
-    protected override void Awake()
-    {
-        base.Awake();
-        onHoverEntered.AddListener(selectGame);
-        onHoverExited.AddListener(exitGame);
-    }
-
-    private void OnDestroy()
-    {
-        onHoverEntered.RemoveListener(selectGame);
-        onHoverExited.RemoveListener(exitGame);
-    }
-
-    private void selectGame(XRBaseInteractor interactor)
-    {
-        //Computer can be turned on when hand is in.
-        reference.action.started += buttonPress;
-    }
-
-    private void exitGame(XRBaseInteractor interactor)
-    {
-        //Make it not possible to turn on computer when hand is not in
-        reference.action.started -= buttonPress;
-    }
-
-    private void buttonPress(InputAction.CallbackContext context)
-    {
-        GameManager.Instance.LoadGame(5);
-    }
-    */
 }
--- CIDSE - An Intelligent Tutoring in Virtual Reality/Assets/Scripts/HallToClass.cs
+++ CIDSE - An Intelligent Tutoring in Virtual Reality/Assets/Scripts/HallToClass.cs
@@ -2,24 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class HallToClass : XRBaseInteractable
 {
+    public InputActionReference reference = null;
+    public GameObject player = null;
+    public GameObject text = null;
+    private float distance;
+    public float accDistance = 2;
+
+
     protected override void Awake()
     {
-        base.Awake();
-        onHoverEntered.AddListener(StartPress);
+        reference.action.started += ToClass;
+        text.SetActive(false);
     }
 
     protected override void OnDestroy()
     {
-        onHoverEntered.RemoveListener(StartPress);
+        reference.action.started -= ToClass;
     }
 
-    private void StartPress(XRBaseInteractor interactor)
+    private void Update()
     {
-        SceneManager.LoadScene(2);
+        distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+        if (distance < accDistance)
+        {
+            text.SetActive(true);
+        }
+        else
+        {
+            text.SetActive(false);
+        }
+    }
+
+    private void ToClass(InputAction.CallbackContext context)
+    {
+        if (distance < accDistance)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
